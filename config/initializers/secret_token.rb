ce6d945931706f9b8af443f6adcf4245b66078ca
Jenkins <james@somehero.com>
2013-03-02 09:50:04 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialpayments::Application.config.secret_token = '32c0c4bb1b17337d74299cab91d46686710592fa514a2d2ef891123a5bfec91cf4e2fc0039e7392a345ec3a2901fcbf5a3096b4ff2ecbb65a68ddfc12225fd2a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Socialpayments::Application.config.session_store :cookie_store, key: '_socialpayments_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Socialpayments::Application.config.session_store :active_record_store
